124d2b5eb93145c4d60c27cae9532f294ac8f3d7
Brian Tiger Chow <brian.holderchow@gmail.com>
2015-02-04 21:46:51 -0800

    feat(repo/identity) put key pair in id and id.pub files
    TODO pass numbits

---
+++ repo/fsrepo/component/identity.go
@@ -0,0 +1,73 @@
+package component
+
+import (
+	"io/ioutil"
+	"os"
+	path "path"
+
+	config "github.com/jbenet/go-ipfs/repo/config"
+	util "github.com/jbenet/go-ipfs/util"
+)
+
+const (
+	filenamePrivateKey = "id"
+	filenamePublicKey  = "id.pub"
+	minBits            = 1024
+)
+
+var _ Component = &IdentityComponent{}
+var _ Initializer = InitIdentityComponent
+var _ InitializationChecker = IdentityComponentIsInitialized
+
+// IdentityComponent manages a public/private keypair
+// NOT THREAD-SAFE
+type IdentityComponent struct {
+	path string // required at instantiation
+}
+
+func InitIdentityComponent(fspath string, conf *config.Config) error {
+	if IdentityComponentIsInitialized(fspath) {
+		return nil
+	}
+	identity, err := config.InitIdentity(ioutil.Discard, minBits)
+	if err != nil {
+		return err
+	}
+	files := [][2]string{
+		[2]string{filenamePublicKey, identity.PeerID},
+		[2]string{filenamePrivateKey, identity.PrivKey},
+	}
+	for _, pair := range files {
+		filename := pair[0]
+		data := []byte(pair[1])
+		if err := ioutil.WriteFile(path.Join(fspath, filename), data, os.ModePerm); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Open returns an error if the config file is not present. This component is
+// always called with a nil config parameter. Other components rely on the
+// config, to keep the interface uniform, it is special-cased.
+func (c *IdentityComponent) Open(_ *config.Config) error {
+	return nil
+}
+
+func (c *IdentityComponent) Close() error {
+	return nil // config doesn't need to be closed.
+}
+
+func (c *IdentityComponent) SetPath(p string) {
+	c.path = p
+}
+
+// IdentityComponentIsInitialized returns true if the id files are present.
+func IdentityComponentIsInitialized(fspath string) bool {
+	for _, name := range []string{filenamePublicKey, filenamePrivateKey} {
+		if !util.FileExists(path.Join(fspath, name)) {
+			return false
+		}
+	}
+	return true
+}
--- repo/fsrepo/fsrepo.go
+++ repo/fsrepo/fsrepo.go
@@ -54,6 +54,7 @@ type FSRepo struct {
 	// the FSRepo is modified.
 	// TODO test
 	configComponent    component.ConfigComponent
+	identityComponent component.IdentityComponent
 	datastoreComponent component.DatastoreComponent
 	eventlogComponent  component.EventlogComponent
 }
@@ -335,6 +336,21 @@ func componentBuilders() []componentBuilder {
 					return err
 				}
 				r.configComponent = c
+				return nil
+			},
+		},
+
+		// IdentityComponent
+		componentBuilder{
+			Init:          component.InitIdentityComponent,
+			IsInitialized: component.IdentityComponentIsInitialized,
+			OpenHandler: func(r *FSRepo) error {
+				c := component.IdentityComponent{}
+				c.SetPath(r.path)
+				if err := c.Open(r.configComponent.Config()); err != nil {
+					return err
+				}
+				r.identityComponent = c
 				return nil
 			},
 		},
