d541bf886f4ef928cf6c34487d6f64deaa2739b3
W. Trevor King <wking@tremily.us>
2015-05-11 20:07:13 -0700

    core/commands/resolve: Add a -r / --recursive option
    For explicitly enabling recursive behaviour (it was previously always
    enabled).  That allows folks who are interested in understanding
    layered indirection to step through the chain one link at a time.

--- core/commands/resolve.go
+++ core/commands/resolve.go
@@ -46,6 +46,9 @@ Resolve te value of another name:
 	Arguments: []cmds.Argument{
 		cmds.StringArg("name", false, false, "The IPNS name to resolve. Defaults to your node's peerID.").EnableStdin(),
 	},
+	Options: []cmds.Option{
+		cmds.BoolOption("recursive", "r", "Resolve until the result is not an IPNS name"),
+	},
 	Run: func(req cmds.Request, res cmds.Response) {
 
 		n, err := req.Context().GetNode()
@@ -75,7 +78,13 @@ Resolve te value of another name:
 			name = req.Arguments()[0]
 		}
 
-		output, err := n.Namesys.Resolve(n.Context(), "/ipns/" + name, 0)
+		recursive, _, _ := req.Option("recursive").Bool()
+		depth := 1
+		if recursive {
+			depth = 0
+		}
+
+		output, err := n.Namesys.Resolve(n.Context(), "/ipns/" + name, depth)
 		if err != nil {
 			res.SetError(err, cmds.ErrNormal)
 			return
